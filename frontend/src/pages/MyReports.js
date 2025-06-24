ccbb5d931f74f08cbca0c3e9b72822dfe83669e0
RPWeiQiang <23013990@myrp.edu.sg>
2025-06-24 14:05:50 +0800

    login

---
+++ frontend/src/pages/MyReports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyReports = () => {
+  return (
+    <div>
+      <h1>My Reports</h1>
+      <p>Your submitted reports will appear here</p>
+    </div>
+  );
+};
+
+export default MyReports;
